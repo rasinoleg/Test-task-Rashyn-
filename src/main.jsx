252c6250eba229749d0971bf476709527d3e01ba
rasinoleg <rasinoleg3@gmail.com>
2023-12-11 21:53:14 +0200

    test-task

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-
 import App from './App.jsx';
 import './index.css';
 import GlobalStyle from './styles/globalStyles';
 import { persistor, store } from './redux/store.js';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    
